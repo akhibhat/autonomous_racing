31039c1f28361080905bf3ecb5a6545c152a4359
Akhilesh <akoo09@seas.upenn.edu>
2020-02-04 12:41:19 -0500

    add wall follow with safety

---
+++ wall_follow/scripts/wall_follower.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+import math
+import numpy as np
+
+#ROS Imports
+import rospy
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped as  ADS
+
+class WallFollow:
+
+    def __init__(self):
+
+        rospy.init_node("wall_follow_node", anonymous=True)
+
+        self.kp = 3.0
+        self.kd = 0.01
+        self.ki = 0.0
+        self.servo_offset = 0.0
+        self.prev_error = 0.0
+        self.error = 0.0
+        self.prev_time = 0.0
+        self.current_time = 0.0
+        self.integral = 0.0
+
+        self.angle_range = 360
+        self.theta = 60
+        self.theta_rad = 1.0472
+        self.right_d = 0.9
+        self.left_d = 0.55
+        self.velocity = 2.0
+        self.car_length = 0.50
+
+        self.lookahead = 0.20
+
+        self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.laserScanCallback)
+        self.drive_pub = rospy.Publisher("/nav", ADS, queue_size = 1)
+
+    def laserScanCallback(self, scan_msg):
+
+        # scan_truncated = self.truncate_scan(scan_msg)
+#        alpha = self.rightWallFollow(scan_msg)
+        alpha = self.leftWallFollow(scan_msg)
+
+        if  abs(alpha) < 0.1745:
+            self.velocity = 1.5
+        elif abs(alpha) > 0.1745 and abs(alpha) < 0.3491:
+            self.velocity = 1.0
+        else:
+            self.velocity = 0.5
+            print(alpha)
+
+        self.pid_control()
+
+    def pid_control(self):
+
+        self.prev_time = self.current_time
+        self.current_time = rospy.Time.now().to_sec()
+
+        drive_msg = ADS()
+        drive_msg.header.stamp = rospy.Time.now()
+        drive_msg.header.frame_id = "laser"
+
+        self.integral = self.integral + self.error
+
+        steering_angle = self.kp*self.error + self.kd*((self.error - self.prev_error)/(self.current_time-self.prev_time)) + self.integral*self.ki
+
+        if math.isnan(steering_angle):
+            print("Steering angle not valid")
+        else:
+            drive_msg.drive.steering_angle = steering_angle
+            drive_msg.drive.speed = self.velocity
+
+        self.drive_pub.publish(drive_msg)
+
+        self.prev_error = self.error
+
+    def rightWallFollow(self, scan_msg):
+
+        zero_index = ((self.angle_range/2) - 90) * (len(scan_msg.ranges)/self.angle_range)
+        theta_index = ((self.angle_range/2) - 90 + self.theta) * (len(scan_msg.ranges)/self.angle_range)
+
+        b = scan_msg.ranges[zero_index]
+        a = scan_msg.ranges[theta_index]
+
+        alpha = math.atan2(a*math.cos(self.theta_rad) - b, a*math.sin(self.theta_rad))
+
+        dt = b*math.cos(alpha) + self.lookahead*math.sin(alpha)
+
+        self.error = dt - self.right_d
+
+        return alpha
+
+    def leftWallFollow(self, scan_msg):
+
+        zero_index = ((self.angle_range/2) + 90) * (len(scan_msg.ranges)/self.angle_range)
+        theta_index = ((self.angle_range/2) + 90 - self.theta) * (len(scan_msg.ranges)/self.angle_range)
+
+        b = scan_msg.ranges[zero_index]
+        a = scan_msg.ranges[theta_index]
+
+        alpha = math.atan2(a*math.cos(self.theta_rad) - b, a*math.sin(self.theta_rad))
+
+        dt = b*math.cos(alpha) + self.lookahead*math.sin(alpha)
+
+        self.error = -(self.left_d - dt)
+
+        # print(self.error)
+
+        return alpha
+
+if __name__ == "__main__":
+    wf = WallFollow()
+    rospy.spin()
---
+++ wall_follow/src/wall_follow.cpp
@@ -0,0 +1,175 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
+
+class WallFollow
+{
+    private:
+        ros::NodeHandle nh_;
+
+        // Publisher
+        ros::Publisher drive_pub_;
+
+        // Subscriber
+        ros::Subscriber scan_sub_;
+
+        double kp_, kd_, ki_;
+
+        double right_d_, left_d_;
+        double integral_;
+        
+        double prev_time_, current_time_;
+        
+        int angle_range_, theta_;
+        double theta_rad_;
+
+        double velocity_;
+        double alpha_;
+        double lookahead_;
+        double error_, prev_error_;
+
+        bool follow_left_, follow_right_;
+
+    public:
+        WallFollow(ros::NodeHandle &nh)
+        {
+            nh_ = nh;
+            
+            // PID variables
+            kp_ = 4.0;
+            kd_ = 0.0;
+            ki_ = 0.0;
+    
+            // Wall follow params
+            right_d_ = 0.9;
+            left_d_ = 0.55;
+            velocity_ = 2.0;
+            angle_range_ = 360;
+            theta_ = 70;
+            theta_rad_ = 1.2217;
+            alpha_ = 0.0;
+            lookahead_ = 2.0;
+    
+            // Other variables
+            prev_error_ = 0.0;
+            error_ = 0.0;
+            integral_ = 0.0;
+            follow_left_ = true;
+            follow_right_ = false;
+            prev_time_ = 0.0;
+            current_time_ = 0.0;
+        }
+
+        ~WallFollow(){}
+
+        void initialize(){
+
+            drive_pub_ = nh_.advertise<ackermann_msgs::AckermannDriveStamped>("/drive", 1);
+
+            scan_sub_ = nh_.subscribe("/scan", 1, &WallFollow::laserScanCallback, this);
+        }
+
+        void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+
+            // make_decision(scan_msg->ranges);
+            if (follow_left_ == true)
+            {
+                followLeft(scan_msg);
+            }
+            else
+            {
+                followRight(scan_msg);
+            }
+
+            pid_control();
+        }
+
+        void followLeft(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+
+            int zero_index, theta_index;
+            double b, a, dt;
+            
+            zero_index = ((angle_range_/2) + 90) * (scan_msg->ranges.size())/angle_range_;
+            theta_index = ((angle_range_/2) + 90 - theta_) * (scan_msg->ranges.size())/angle_range_;
+
+            b = scan_msg->ranges[zero_index];
+            a = scan_msg->ranges[theta_index];
+
+            alpha_ = std::atan2(a * cos(theta_rad_) - b, a * sin(theta_rad_));
+
+            dt = b * cos(alpha_) + lookahead_ * sin(alpha_);
+
+            error_= -(left_d_ - dt);
+            
+            std::cout << dt << "\n";
+        }
+        
+        void followRight(const sensor_msgs::LaserScan::ConstPtr& scan_msg){
+
+            int zero_index, theta_index;
+            double b, a, dt;
+            
+            zero_index = ((angle_range_/2) - 90) * (scan_msg->ranges.size())/angle_range_;
+            theta_index = ((angle_range_/2) - 90 + theta_) * (scan_msg->ranges.size())/angle_range_;
+
+            b = scan_msg->ranges[zero_index];
+            a = scan_msg->ranges[theta_index];
+
+            alpha_ = std::atan2(a * cos(theta_rad_) - b, a * sin(theta_rad_));
+
+            dt = b * cos(alpha_) + lookahead_ * sin(alpha_);
+
+            error_= right_d_ - dt;
+        }
+
+        void pid_control(){
+            
+            prev_time_ = current_time_;
+            current_time_ = ros::Time::now().toSec();
+
+            double time_diff = current_time_ - prev_time_;
+
+            integral_ += error_;
+            double steering_angle = kp_*error_; //+ ki_*integral_ + kd_*((error_ - prev_error_)/time_diff);
+
+            ackermann_msgs::AckermannDriveStamped drive_msg;
+            drive_msg.header.stamp = ros::Time::now();
+            drive_msg.header.frame_id = "laser";
+
+            if(std::isnan(steering_angle)){
+                drive_msg.drive.speed = 0;
+                drive_msg.drive.steering_angle = 0;
+                ROS_ERROR("Invalid steering angle! Cannot be nan");
+            }
+
+            drive_msg.drive.steering_angle = steering_angle;
+
+            if (abs(steering_angle) < 0.1745){
+                drive_msg.drive.speed = 1.5;
+            }
+            else if(abs(steering_angle) > 0.1745 && abs(steering_angle) < 0.3491){
+                drive_msg.drive.speed = 1.0;
+            }
+            else{
+                drive_msg.drive.speed = 0.5;
+            }
+
+            drive_pub_.publish(drive_msg);
+
+            prev_error_ = error_;
+        }
+};
+
+int main(int argc, char **argv){
+    
+    ros::init(argc, argv, "wallFollow");
+
+    ros::NodeHandle nh;
+
+    WallFollow wf(nh);
+    wf.initialize();
+
+    ros::spin();
+
+    return 0;
+}
